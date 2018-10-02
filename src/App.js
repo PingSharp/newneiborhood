7705e496b65072df6535099df6c865f183798600
KePing Zang <dotping@online.de>
2018-10-02 17:04:28 +0200

    feature: add menu button for small screen

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import Map from './Map';
 
 const mapHeight = window.innerHeight;
 class App extends Component {
+  showMenu(){
+    let list = document.querySelector(".ListContainer");
+    if(list.classList.contains("hide")){
+      list.classList.remove("hide");
+      list.classList.add("show")
+    }
+    else{
+      list.classList.remove("show");
+      list.classList.add("hide");
+    }
+  }
   render() {
     return (
       <div className="App" style={{ height: mapHeight }} >
+      <button id='menuItem' onClick={this.showMenu}>menu</button>
         <Map/>
       </div>
     );
