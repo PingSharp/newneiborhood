936c11623ef974c17ed918cd8c0a902148c23ef3
KePing Zang <dotping@online.de>
2018-10-08 15:33:19 +0200

    refactor: change ListView component to static component

--- src/ListView.js
+++ src/ListView.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 const mapHeight = window.innerHeight;
-class ListView extends Component {
-    render(){
+const ListView = ({
+    findArea,
+    locations,
+    showInfoWindow
+}) => {
         return(
             <div  className="ListContainer hide" style={{height: mapHeight}}>
                 <h1 tabIndex="0"  role="heading">Esslingen Location</h1>
-                <select tabIndex="0"  role="menu" id="search" defaultValue={'select'} onChange={this.props.findArea}>
+                <select tabIndex="0"  role="menu" id="search" defaultValue={'select'} onChange={findArea}>
                  <option  value='select' disabled>select...</option>
                   <option role="menuitem" value='All'>All</option>
                   <option role="menuitem" value='Tourist Attraction'>Tourist Attraction</option> 
@@ -15,14 +18,14 @@ class ListView extends Component {
                   <option role="menuitem" value='supermarket'>supermarket</option>
                 </select>
                 <ul   className="List">
-                    {this.props.locations.map((location)=>{
-                        return <li role="link" tabIndex="0" className="ListItem" key={location.id}><a onClick={(event)=>{this.props.showInfoWindow(event.target.text)}}>{location.title}</a></li>
+                    {locations.map((location)=>{
+                        return <li role="link" tabIndex="0" className="ListItem" key={location.id}><a onClick={(event)=>{showInfoWindow(event.target.text)}}>{location.title}</a></li>
                     })}
                 </ul>
             </div>
         )
     }
     
-}
+
 
 export default ListView;
